e8704843cc66bc4f01ab73ff1eab69715a4ec653
rilwanat <49300698+rilwanat@users.noreply.github.com>
2025-03-27 18:38:17 +0100

    update dependencies

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function LandingPage({ 
+    isMobile, isMenuOpen, toggleMenu, closeMenu
+ }) {
+
+
+    return (
+        <div>
+            Hi
+        </div>
+    );
+
+
+ }
