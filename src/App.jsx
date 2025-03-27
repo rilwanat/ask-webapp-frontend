f6af8220db0c7df094ed3fbfa97b0341b9d4b3e5
rilwanat <49300698+rilwanat@users.noreply.github.com>
2025-03-27 18:24:37 +0100

    first commit and dependency installs

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import LandingPage from './components/LandingPage.jsx';
+
+function App() {
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1000);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1000);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <Router>
+      <div>
+        <div>
+
+          <Routes>
+
+            <Route path="/*" element={<div>NOT FOUND</div>} />            
+            <Route path='/' element={<LandingPage isMobile={isMobile} isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} closeMenu={closeMenu} />}/>
+
+            </Routes>
+            
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+export default App
